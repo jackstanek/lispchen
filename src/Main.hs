70e846ffa41b3ac8a729d1869bb5fb8ff86b615d
Jack Stanek <stane064@umn.edu>
2019-11-21 00:46:45 -0600

    Begin progress on evaluation

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,20 @@
 import System.Console.Haskeline
 import System.IO
 import System.Environment
-import Text.ParserCombinators.ReadP
+import Text.ParserCombinators.Parsec
 
-data Atom = IntVal Integer |
-            StringVal String |
-            SymbolVal String |
-            TrueLit |
-            Nil
+data Atom = IntVal Integer
+          | StringVal String
+          | SymbolVal String
+          | TrueLit
+          | Nil
+          | Quoted Sexp
 
-data Sexp = Value Atom | Cons Atom Sexp
+data Sexp = Value Atom | Cons Sexp Sexp
+
+testSexp = (Cons (Value (SymbolVal "yolo"))
+             (Cons (Value (IntVal 4)) (Value (IntVal 20))))
+
 
 main :: IO ()
 main =
@@ -24,13 +29,18 @@ main =
         Nothing -> return ()
         Just ":quit" -> return ()
         Just line -> do
-          outputStrLn . ("=> " ++) . reprSexp . parseSexp $ line
+          outputStrLn . ("=> " ++) . reprSexp $ testSexp
           loop
 
-parseSexp :: String -> Sexp
-parseSexp str = Value (SymbolVal "foo")
+evalSexp :: Sexp -> Sexp
+evalSexp (Value a) = Value a
+evalSexp (Cons left right) =
+  Cons left right
 
+innerTerms :: Sexp -> String
 reprAtom :: Atom -> String
+
+-- Get representation of an atom.
 reprAtom atom =
   case atom of
     IntVal i -> show i
@@ -38,12 +48,16 @@ reprAtom atom =
     SymbolVal s -> s
     TrueLit -> "t"
     Nil -> "nil"
+    Quoted s -> case s of
+                  Value v -> reprAtom v
+                  otherwise -> "'" ++ reprSexp s
 
+-- Get a representation of the contents of a cons.
+innerTerms (Value v) = reprAtom v
+innerTerms (Cons left right) = reprSexp left ++ " " ++ innerTerms right
+
+-- Get a representation of a
 reprSexp :: Sexp -> String
 reprSexp (Value atom) = reprAtom atom
-reprSexp (Cons atom sexp) =
-  "(" ++ reprAtom atom ++ " " ++ innerTerms sexp ++ ")"
-  where innerTerms s =
-          case s of
-            Value a -> reprAtom a
-            Cons a s' -> reprAtom a ++ " " ++ innerTerms s'
+reprSexp (Cons left right) =
+  "(" ++ reprSexp left ++ " " ++ innerTerms right ++ ")"
