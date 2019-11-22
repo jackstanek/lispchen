efac41d9eb93183e4c82d75176f3804d43f3b7ae
Jack Stanek <stanekjack@gmail.com>
2019-11-21 19:28:10 -0600

    Add variable lookups

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,25 @@
 -- Copyright 2019, Jack Stanek. All rights reserved.
 
+import qualified Data.Map.Lazy as Map
+import Data.Maybe
 import System.Console.Haskeline
 import System.IO
 import System.Environment
 import Text.ParserCombinators.Parsec
 
+type Symbol = String
 data Atom = IntVal Integer
           | StringVal String
-          | SymbolVal String
+          | SymbolVal Symbol
           | TrueLit
           | Nil
           | Quoted Sexp
 
 data Sexp = Value Atom | Cons Sexp Sexp
+type Env = Map.Map Symbol Atom
 
 testSexp = (Value (Quoted (Cons (Value (SymbolVal "yolo"))
              (Cons (Value (IntVal 4)) (Value (IntVal 20))))))
-
 
 main :: IO ()
 main =
@@ -29,12 +32,18 @@ main =
         Nothing -> return ()
         Just ":quit" -> return ()
         Just line -> do
-          outputStrLn . ("=> " ++) . reprSexp $ testSexp
+          outputStrLn . ("=> " ++) . reprSexp . topLevelEval $ testSexp
           loop
+            where topLevelEval = evalSexp (Map.singleton "foo" (IntVal 420))
 
-evalSexp :: Sexp -> Sexp
-evalSexp (Value a) = Value a
-evalSexp (Cons left right) =
+evalSexp :: Env -> Sexp -> Sexp
+evalSexp env (Value atom) =
+  Value (case atom of
+           SymbolVal sym ->
+             -- TODO: Error out on lookup failure!
+             fromMaybe Nil $ Map.lookup sym env
+           otherwise -> atom)
+evalSexp env (Cons left right) =
   Cons left right
 
 
